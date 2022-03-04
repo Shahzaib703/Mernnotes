7262cf2ef1c9a97a1bdec6c59ef49a4dc3a95257
Shahzaib703 <98797871+Shahzaib703@users.noreply.github.com>
2022-03-04 13:02:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 5000;
 require("./db/conn");
 const router = require('./routers');
 
+app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.json());
 app.use(router);
